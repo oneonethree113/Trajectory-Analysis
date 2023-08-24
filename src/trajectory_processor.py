74abbeb0d15b0dd7b7dce80e60170c8c8b3bba44
Henry Li <32378441+oneonethree113@users.noreply.github.com>
2023-08-25 01:47:22 +1000

    Add generateDistanceMapForLargeData
    The execution time will be shorter than the original one when dataset has more than 3000 trajectories.

--- src/trajectory_processor.py
+++ src/trajectory_processor.py
@@ -2,20 +2,20 @@ from collections import Counter
 import numpy as np
 from scipy.cluster.hierarchy import linkage, fcluster
 
-def getBigCluster(clusters: list, numMostCommonGroupToDisplay: int) -> dict:
+def getBigCluster(clusters: np.ndarray, numMostCommonGroupToDisplay: int) -> dict:
     """
     Identify and return the largest clusters from a list of clusters.
 
     Args:
-        clusters (list): List of cluster assignments for data points.
+        clusters (np.ndarray): List of cluster assignments for data points.
         numMostCommonGroupToDisplay (int): Number of most common clusters to display.
 
     Returns:
         dict: Dictionary containing the largest clusters and their member indices.
     """
     
-    if not isinstance(clusters, list):
-        raise TypeError("clusters must be a list.")
+    if not isinstance(clusters, np.ndarray):
+        raise TypeError("clusters must be a np.ndarray.")
     if not isinstance(numMostCommonGroupToDisplay, int):
         raise TypeError("numMostCommonGroupToDisplay must be a int.")
     clusterMembers = {}  # Create a dictionary to store cluster memberships
@@ -88,6 +88,75 @@ def generateDistanceMap(parsedTrajectoryList: list, windowSize: int) -> np.ndarr
             if i > j:
                 continue
             distance = 1 - entity.similarity(entity2, windowSize)  # Calculate the similarity
+            distanceMap[i][j] = distance
+            distanceMap[j][i] = distance
+
+    print(f'All trajectories have been processed')
+
+    return distanceMap
+
+def generateDistanceMapForLargeData(parsedTrajectoryList: list, windowSize: int) -> np.ndarray:
+    """
+    Generate a similarity-based distance map for a list of trajectories using the concept of Dilation in image processing.
+    The distance is calculated based on how many coordinates of both trajectories are near each other.
+    The execution time will be shorter when dataset has more than 3000 prajectories.
+
+    Args:
+        parsedTrajectoryList (list): A list of Trajectory instances.
+        windowSize (int): The size of the window for similarity calculation.
+
+    Returns:
+        numpy.ndarray: A NumPy array containing the similarity-based distance map.
+    """
+    if not isinstance(parsedTrajectoryList, list):
+        raise TypeError("parsedTrajectoryList must be a list.")
+    if not isinstance(windowSize, int):
+        raise TypeError("windowSize must be a int.")
+    print(f'There are {len(parsedTrajectoryList)} trajectories in the dataset')
+
+    distanceMap = np.zeros((len(parsedTrajectoryList), len(parsedTrajectoryList)))
+
+    xMaxList = []
+    yMaxList = []
+
+    # Calculate the maximum x and y coordinates 
+    for i, entity in enumerate(parsedTrajectoryList):
+        x_coords, y_coords = entity.trajExport()
+        xMaxList.append(max(x_coords))
+        yMaxList.append(max(y_coords))
+
+    xMax = max(xMaxList)
+    yMax = max(yMaxList)
+
+    trajAreaList = []
+
+    # Calculate the area occupied by each trajectory in the space
+    for i, entity in enumerate(parsedTrajectoryList):
+
+        areaMap = np.zeros((int(xMax + windowSize/2) + 1, int(yMax + windowSize/2) + 1))
+        x_coords, y_coords = entity.trajExport()
+
+        for x, y in zip(x_coords, y_coords):
+            areaMap[int(x - windowSize/2):int(x + windowSize/2), int(y - windowSize/2):int(y + windowSize/2)] = 1
+
+        trajAreaList.append(areaMap)
+
+    for i, entity in enumerate(parsedTrajectoryList):
+        disArray = []  # Initialize an array to store distances from the current entity
+
+        if i % 10 == 0:
+            print(f'{i} of {len(parsedTrajectoryList)} trajectories has been processed')
+
+        # Calculate similarity with all other trajectories
+        for j, entity2 in enumerate(parsedTrajectoryList):
+            if i > j:
+                continue
+            
+            # Calculate the difference in areas and the similarity distance
+            diff = np.absolute(trajAreaList[i] - trajAreaList[j])
+            distance = np.sum(diff) / (np.sum(trajAreaList[i]) + np.sum(trajAreaList[j]))
+
+            # Assign the calculated distance to the distance map
             distanceMap[i][j] = distance
             distanceMap[j][i] = distance
 
